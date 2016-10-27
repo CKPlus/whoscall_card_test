25ac1d10c561db4433c1a3939443fed833d71eab
nicoleyang0319 <nicoleyang@Gogolook-MacBook-Air-14.local>
2016-10-27 12:17:11 +0800

    Merge Code

--- test/specs/whoscallcard_index.js
+++ test/specs/whoscallcard_index.js
@@ -16,5 +16,23 @@ describe('whoscallcard index page', function() {
        assert.equal(title, 'Whoscall Card - Your Personal Business Card');
    });
 
+  it('click multilanguage botton KR', function ()  {
+     browser.selectByValue('body > footer > div > div.copyright > ul > li > span.lang-selector-container > select' ,'ko-KR');
+     var title = browser.getTitle();
+     assert.equal(title,'후스콜카드 | Whoscall 개인 명함 서비스');
+  });
+
+  it('click multilanguage botton TH', function ()  {
+     browser.selectByValue('body > footer > div > div.copyright > ul > li > span.lang-selector-container > select' ,'th-TH');
+     var title = browser.getTitle();
+     assert.equal(title,'Whoscall Card - นามบัตรส่วนตัวของคุณ');
+  });
+
+  it('click multilanguage botton BR', function ()  {
+     browser.selectByValue('body > footer > div > div.copyright > ul > li > span.lang-selector-container > select' ,'pt-BR');
+     var title = browser.getTitle();
+     assert.equal(title,'Cartão Whoscall - Seu cartão de visitas pessoal e de negócios');
+  });
+
 });
 
